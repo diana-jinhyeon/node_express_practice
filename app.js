e15fc23f80c0500b51050ce0688650da37c23fca
diana-jinhyeon <diana.jinhyeon@gmail.com>
2020-03-30 19:47:53 +0900

    Initial commit

---
+++ app.js
@@ -0,0 +1,35 @@
+var express = require('express')
+var app = express()
+var bodyParser = require('body-parser')
+var cors = require('cors')
+
+
+app.listen(3000, function(){
+  console.log("start! express server on port 3000!")
+});
+
+app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.set('view engine', 'ejs');
+app.use(cors())
+
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + "/public/main.html");
+});
+
+app.get('/main', function(req, res){
+  res.sendFile(__dirname + "/public/main.html");
+});
+
+app.post('/email_post', function(req, res){
+  console.log(req.body);
+  res.render('email.ejs', {'email': req.body.email});
+});
+
+app.post('/ajax_send_email', function(req, res){
+  console.log(req.body.email);
+  var responseData = {'result':'ok', 'email': req.body.email}
+  res.json(responseData)
+});
